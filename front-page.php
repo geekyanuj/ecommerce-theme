89f4953134fbd846e4fe4590770806f56261c691
Anuj Kumar <anujv8692@gmail.com>
2026-06-27 21:19:59 +0530

    first commit

---
+++ front-page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php get_template_part('template-parts/hero'); ?>
+
+<?php get_template_part('template-parts/certifications'); ?>
+
+<?php get_template_part('template-parts/story'); ?>
+
+<?php get_template_part('template-parts/categories'); ?>
+
+<?php get_template_part('template-parts/featured-products'); ?>
+
+<?php get_template_part('template-parts/reviews'); ?>
+
+<?php get_footer(); ?>
